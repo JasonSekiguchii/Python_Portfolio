6ad1e80878114368e9375e5ef263632e2e6efd63
Jason <jason.seki@gmail.com>
2023-06-28 08:57:40 -0700

    Add files via upload

---
+++ NumPy_Grocery_list.py
@@ -0,0 +1,18 @@
+﻿import numpy as np
+
+cupcakes = np.array([2, 0.75, 2, 1, 0.5])
+
+recipes = np.genfromtxt('recipes.csv', delimiter=',')
+#print(recipes)
+
+eggs = recipes[:, 2]
+one_egg = recipes[(eggs == 1)]
+#print(eggs)
+#print(one_egg)
+
+cookies = recipes[2,:]
+#print(cookies)
+
+double_batch = cupcakes * 2
+grocery_list = cookies + double_batch
+print(grocery_list)
